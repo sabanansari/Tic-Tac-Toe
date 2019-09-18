3a00af1561474fb14a17b0cbd023141d3f369105
sabanansari <alisaban52@gmail.com>
2019-09-19 03:33:20 +0530

    implemented logic of winning tic-tac-toe

--- app/src/main/java/com/example/my/connect3/MainActivity.java
+++ app/src/main/java/com/example/my/connect3/MainActivity.java
@@ -3,8 +3,57 @@ package com.example.my.connect3;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    //0:yellow, 1:red 2:empty
+
+    int activePlayer = 0;
+
+    boolean gameActive = true;
+    int[] gameState ={2,2,2,2,2,2,2,2,2};
+
+    int[][] winningPositions = {{1,8,7},{6,5,4},{3,2,9},{1,6,3},{8,5,2},{7,4,9},{2,4,6},{0,4,8}};
+
+    public void dropin(View view){
+
+        ImageView counter = (ImageView) view;
+
+        int tappedCounter = Integer.parseInt(counter.getTag().toString());
+
+        if(gameState[tappedCounter]==2 && gameActive) {
+
+            gameState[tappedCounter] = activePlayer;
+
+            counter.setTranslationY(-1500);
+
+            if (activePlayer == 0) {
+                counter.setImageResource(R.drawable.yellow);
+                activePlayer = 1;
+            } else {
+                counter.setImageResource(R.drawable.red);
+                activePlayer = 0;
+            }
+            counter.animate().translationYBy(1500).rotation(3600).setDuration(500);
+
+            for (int[] winningPosition : winningPositions) {
+                if (gameState[winningPosition[0]] == gameState[winningPosition[1]] && gameState[winningPosition[1]] == gameState[winningPosition[2]] && gameState[winningPosition[0]] != 2) {
+                    gameActive = false;
+                    String winner = "";
+
+                    if (activePlayer == 1) {
+                        winner = "Yellow";
+                    } else {
+                        winner = "Red";
+                    }
+                    Toast.makeText(this, winner + " has won!", Toast.LENGTH_LONG).show();
+                }
+            }
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
